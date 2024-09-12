8d87211beef0a20305ca681ddd93302484e2d0d0
joan-ross <rossj2904@gmail.com>
2024-09-12 18:49:22 -0400

    attempt 2

--- fruitsales.py
+++ fruitsales.py
@@ -1,8 +1,10 @@
 # add your code here
 import pandas as pd
 
-pd.DataFrame({'Apples':[35,21],
-               'Bananas':[41,34]},
-               index =['2017 Sales','2018 Sales'])
+fruit_sales = pd.DataFrame({'Apples':[35,21],
+                            'Bananas':[41,34]},
+                            index =['2017 Sales','2018 Sales'])
+
+fruit_sales.to_csv("fruits_sales.csv")
 
 
